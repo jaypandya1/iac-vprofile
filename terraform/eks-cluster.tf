dd514973816efd2e11b1ee835e41dc2f3937c937
jaypandya1 <pandyajay2121@gmail.com>
2026-04-05 20:48:11 +0530

    Update Terraform configurations for EKS version and improve GitHub Actions comments

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,23 +1,25 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "~> 21.16.0" # Latest version in April 2026
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  cluster_version = "1.34" # Modern stable version
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+  # New in v20/v21: Automatically grants the creator (you/Terraform) admin rights
+  enable_cluster_creator_admin_permissions = true
 
+  eks_managed_node_group_defaults = {
+    # Updated to the modern Amazon Linux 2023
+    ami_type = "AL2023_x86_64_STANDARD"
   }
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-
       instance_types = ["t3.small"]
 
       min_size     = 1
@@ -27,7 +29,6 @@ module "eks" {
 
     two = {
       name = "node-group-2"
-
       instance_types = ["t3.small"]
 
       min_size     = 1
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -11,7 +11,7 @@ terraform {
     }
 
     tls = {
-      source  = "hashicorp/tls"
+      source  = "hashicorp/tls" ##
       version = "~> 4.0.6"
     }
 
