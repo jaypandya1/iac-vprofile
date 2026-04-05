90161aba6ae8a005083ed8d1a0bd9c8ac246b33c
jaypandya1 <pandyajay2121@gmail.com>
2026-04-05 21:20:18 +0530

    Fix module block formatting in EKS configuration

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,30 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.16.0" # This MUST be inside the module block
-
-  cluster_name    = local.cluster_name
-  cluster_version = "1.34"
-
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2023_x86_64_STANDARD"
-  }
-
-  eks_managed_node_groups = {
-    one = {
-      name           = "node-group-1"
-      instance_types = ["t3.small"]
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
-    }
-  }
-}module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.16.0" # This MUST be inside the module block
+  version = "~> 21.16.0" 
 
   cluster_name    = local.cluster_name
   cluster_version = "1.34"
